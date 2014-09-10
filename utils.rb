b1213d367dbb4067b5ddb4c4a68623217c6e241e
Phil Tsarik <phil.tsarik@gmail.com>
2014-09-10 16:04:02 +0300

    Add caching in Utils methods

--- utils.rb
+++ utils.rb
@@ -1,18 +1,20 @@
 module Utils
 
   def left_opponent?
-    netx = world.get_opponent_player.net_left
-    netx < world.width/2
+    return @left_opponent unless @left_opponent.nil?
+    @left_opponent = world.get_opponent_player.net_left < world.width/2
   end
 
   def opponent_net_center_x
+    return @opponent_net_center_x if @opponent_net_center_x
     opponent = world.get_opponent_player
-    0.5 * (opponent.net_left + opponent.net_right)
+    @opponent_net_center_x = 0.5 * (opponent.net_left + opponent.net_right)
   end
 
   def opponent_net_center_y
+    return @opponent_net_center_y if @opponent_net_center_y
     opponent = world.get_opponent_player
-    0.5 * (opponent.net_top + opponent.net_bottom)
+    @opponent_net_center_y = 0.5 * (opponent.net_top + opponent.net_bottom)
   end
 
 end
