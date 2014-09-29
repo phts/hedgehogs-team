0cb26aa46062546c8ae104d8b236ff439732b1bf
Phil Tsarik <phil.tsarik@gmail.com>
2014-09-29 18:22:36 +0300

    Remove redundant `class << self` statement for a single method

--- my_strategy/constants.rb
+++ my_strategy/constants.rb
@@ -35,22 +35,19 @@ class Constants
     }
   end
 
-  class << self
-
-    def init(game, env)
-      return if $constants_initialized
-      $constants_initialized = true
-      $game = game
-      $rink_center_x = (game.rink_right+game.rink_left) * 0.5
-      $rink_center_y = (game.rink_bottom+game.rink_top) * 0.5
-      $opponent_on_the_left = env.opponent_player.net_left < rink_center_x
-      $opponent_net_center_x = opponent_on_the_left? ? env.opponent_player.net_right : env.opponent_player.net_left
-      $opponent_net_center_y = 0.5 * (env.opponent_player.net_top + env.opponent_player.net_bottom)
-      $my_net_center_x = opponent_on_the_left? ? env.my_player.net_left : env.my_player.net_right
-      $my_net_center_y = 0.5 * (env.my_player.net_top + env.my_player.net_bottom)
-      $enough_pass_angle = 0.5 * game.pass_sector
-      $three_by_three = env.world.hockeyists.size >= 8
-    end
-
+  def self.init(game, env)
+    return if $constants_initialized
+    $constants_initialized = true
+    $game = game
+    $rink_center_x = (game.rink_right+game.rink_left) * 0.5
+    $rink_center_y = (game.rink_bottom+game.rink_top) * 0.5
+    $opponent_on_the_left = env.opponent_player.net_left < rink_center_x
+    $opponent_net_center_x = opponent_on_the_left? ? env.opponent_player.net_right : env.opponent_player.net_left
+    $opponent_net_center_y = 0.5 * (env.opponent_player.net_top + env.opponent_player.net_bottom)
+    $my_net_center_x = opponent_on_the_left? ? env.my_player.net_left : env.my_player.net_right
+    $my_net_center_y = 0.5 * (env.my_player.net_top + env.my_player.net_bottom)
+    $enough_pass_angle = 0.5 * game.pass_sector
+    $three_by_three = env.world.hockeyists.size >= 8
   end
+
 end
